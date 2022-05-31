ee821d078a0b3fa1808035623f95a3cabeff0c00
roman <fdn3445@gmail.com>
2022-06-01 01:39:06 +0300

    Test

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import datetime
 
-year: int = input('vvedite god ')
-month: int = input('vvedite mesiats ')
-day: int = input('vvedite den ')
+year = int(input('vvedite god '))
+month = int(input('vvedite mesiats '))
+day = int(input('vvedite den '))
 
-date_object = datetime.datetime(year,month,day)
-print(date_object)
+date_object = datetime.datetime(year, month, day)
+date_today = datetime.datetime.today()
+delta = date_today - date_object
+print(delta)
