858507f649566e0ef329e2ce08c3bb035ba485f7
Roman <fdn2327@gmail.com>
2022-06-02 18:06:29 +0300

    Test from office

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ delta = date_today - date_object
 print(delta)
 '''
 
+'''
 year_start = int(input('vvedite god pervoi daty '))
 month_start = int(input('vvedite mesiats pervoi daty '))
 day_start = int(input('vvedite den pervoi daty '))
@@ -23,3 +24,8 @@ date_first = datetime.datetime(year_start, month_start, day_start)
 date_fin = datetime.datetime(year_end, month_end, day_end)
 delta = date_fin - date_first
 print(delta)
+'''
+
+now = datetime.datetime.now()
+year = datetime.datetime.strftime(now,'%B')
+print(year)
